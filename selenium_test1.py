c4a1fbaa78e8b23137364c61f97d3e209484abfd
Daicho_0 <lbeduardo8@gmail.com>
2024-09-26 10:25:30 -0500

    Data post get solved

--- selenium_test1.py
+++ selenium_test1.py
@@ -24,8 +24,9 @@ from selenium.webdriver.chrome.service import Service
 #driver = webdriver.Chrome()
 chrome_options = Options()
 chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--disable-dev-shm-usage')
-driver = webdriver.Chrome()
+chrome_options.add_argument("--disable-notifications")
+#chrome_options.add_argument('--disable-dev-shm-usage')
+driver = webdriver.Chrome( options= chrome_options)
 driver.maximize_window()
 #options = webdriver.ChromeOptions()
 #options.add_argument('--headless=new')
@@ -49,71 +50,92 @@ username.send_keys(valor_parametro1)
 password.send_keys(valor_parametro2)
 login = driver.find_element("css selector", "button[type='submit']").click()
 resp = driver.page_source 
-time.sleep(2)
+
 
 driver.get('https://www.facebook.com/groups/2901591359932748')
 
 def obtener_hijos():
+    try:
     
-    feed_div = driver.find_element("css selector", "div[role='feed']")
-    
-    return feed_div.find_elements(By.CSS_SELECTOR, "div")
+        feed_div = driver.find_element("css selector", "div[role='feed']")
+        WebDriverWait(driver, 10).until(
+            EC.visibility_of_all_elements_located(
+                (By.CSS_SELECTOR, "div[class='x1yztbdb x1n2onr6 xh8yej3 x1ja2u2z']")
+            )                                 
+        )
+        return feed_div.find_elements(By.CSS_SELECTOR, "div[class='x1yztbdb x1n2onr6 xh8yej3 x1ja2u2z']")
+    except Exception as e:                                         
+        print(f"Error al obtener hijos: {e}")
+        return []
 
 def scroll_hasta_el_final(driver):
+    # Obtener la altura total de la página
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    time.sleep(2)
-    
-clase_objetivo = "x1yztbdb x1n2onr6 xh8yej3 x1ja2u2z"
-contador = 0
-max_intentos = 3
-intentos = 0
-i=0
-event=True
-while event:
-    try:
-        scroll_hasta_el_final(driver)
-        
-        divs = obtener_hijos()
-        # Procesar los nuevos divs
-        for div in divs:
-            scroll_hasta_el_final(driver)
-            try:
-                class_attr = div.get_attribute("class")
+    time.sleep(5) 
 
-                
-                contador += 1
-                print(f"Div encontrado con la clase objetivo. Contador: {contador}")
+
+def extraer_datos():
+    elementos_vistos = set()
+    event =True  
+    while event:  # Bucle infinito hasta que se detenga manualmente
+          # Realiza scroll para cargar más contenido
+        scroll_hasta_el_final(driver)
+        divs = obtener_hijos()  # Obtiene los elementos actuales
+        i =0
+        for div in divs:
+            texto = div.text
+            i+=1
+            print('cantidad total de divs: ',len(divs))
+            if texto not in elementos_vistos:  # Verifica si el texto ya fue procesado
+                elementos_vistos.add(texto) 
+                #WebDriverWait(driver, 20).until(
+                #EC.presence_of_element_located((By.XPATH, f"(//div[@class='xu06os2 x1ok221b']//span/h2/span)[{i}]"))
+                #)
                 try:
-                        div_global_info_post =driver.find_element(By.CSS_SELECTOR, "div.x1n2onr6.x1ja2u2z > div:not([class]) > div:not([class]) > div > div > div > div > div > div > div:not([class]) > div > div")
+                    div_global_info_post =div.find_element(By.CSS_SELECTOR, "div.x1n2onr6.x1ja2u2z > div:not([class]) > div:not([class]) > div > div > div > div > div > div > div:not([class]) > div > div")
+                    div_cabecera_post = div_global_info_post.find_element(By.CSS_SELECTOR, "div.html-div.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.xexx8yu.x4uap5.x18d9i69.xkhd6sd > div > div.html-div.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.xexx8yu.x4uap5.x18d9i69.xkhd6sd.x1iyjqo2 > div")
+                    div_post_person_name = div_cabecera_post.find_element(By.CSS_SELECTOR, ".xu06os2.x1ok221b span > h2 > span")
+                    div_post_id = div_global_info_post.find_element(By.CSS_SELECTOR, ".xu06os2.x1ok221b span > h2").get_attribute('id')
+
+                    print('estamons en el iterador', i)
+                    print('div: ',div_post_person_name.text)
+                    print('id_post: ', div_post_id)
+                    yield  texto
+                except Exception as e:
+                     print(f"Error al procesar el elemento: {e}")
+                
+            if i== 100 | i==len(divs):
+                print('al final i : ' ,i)
+                event=False
+                break;
+        
+        
+
+for dato in extraer_datos():
+    print('next :')
+
+        
+
+
+#nombres = []  # Lista para almacenar los datos
+#data_generator = extraer_datos()
+
+
+    
+
+'''div_global_info_post =driver.find_element(By.CSS_SELECTOR, "div.x1n2onr6.x1ja2u2z > div:not([class]) > div:not([class]) > div > div > div > div > div > div > div:not([class]) > div > div")
                         
                         div_cabecera_post = div_global_info_post.find_element(By.CSS_SELECTOR, "div.html-div.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.xexx8yu.x4uap5.x18d9i69.xkhd6sd > div > div.html-div.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.xexx8yu.x4uap5.x18d9i69.xkhd6sd.x1iyjqo2 > div")
-
                         #div_cabecera_post= div_global_info_post.find_element(By.XPATH, ".//div[@class='html-div xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd']/div/div[@class='html-div xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x1iyjqo2']/div")
                         #div_post_person_name= div_cabecera_post.find_element(By.XPATH, ".//div[@class='xu06os2 x1ok221b']/span/h2")
-                        div_post_person_name = div_cabecera_post.find_element(By.CSS_SELECTOR, "div.xu06os2.x1ok221b > span > h2")
+                        div_post_person_name = div_cabecera_post.find_element(By.CSS_SELECTOR, "div.xu06os2.x1ok221b > span > h2 > span > span > a ")
 
                         #href_value = div_cabecera_post.find_element(By.XPATH, ".//div[@class='xu06os2 x1ok221b']/span/div/span/span/span/a").get_attribute("href")
 
                         print("username: ",  div_post_person_name.text)
                         #print("enlace: ",  href_value)
 
-                except Exception as e:
-                        print(f"Error al buscar en el árbol del div: {e}")
-                i+=1
-                print("termina en", 50-i)
-                if i > 100: 
-                    # Salir si el contador alcanza 50
-                    event=False 
-                    break
-            
-            except Exception as e:
-                print(f"Error al procesar div: {e}")
-       
-    except Exception as e:
-        print(f"Error durante el scroll o la obtención de divs: {e}")
-        intentos += 1
-        
-print(f"Valor final de i después del bucle: {i}")
-driver.quit()
+               )'''
+#driver.quit()
 
 
